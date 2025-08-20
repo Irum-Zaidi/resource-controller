3d22cfbd7dbc47fa9105651ae6a7c756dfbc06da
Irum Zaidi <irum@artynode.com>
2025-08-20 22:34:25 +0500

    Resource Controller (30/64 lecture)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::resource('users', UserController::class);
+// Route::resource('users', UserController::class)->only([
+//     'create', 'show', 'update'
+// ]);
+// Route::resource('users', UserController::class)->except([
+//     'create', 'show', 'update'
+// ]);
+// Route::resource('users', UserController::class);
+// Route::resource('users', UserController::class)->names([
+
+//     'create' => 'users.build',
+//     'show' => 'users.view',
+// ]);
+
+
+// Route::resource('users.comments', CommentController::class);
+Route::resource('users.comments', CommentController::class)->shallow();
